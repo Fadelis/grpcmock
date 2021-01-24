51102484337329b8deedb24361f807cd4eff3890
Fadelis <paulius.papl@gmail.com>
2021-01-24 18:09:17 +0100

    fixes typo

--- grpcmock-core/src/main/java/org/grpcmock/definitions/matcher/steps/StreamRequestMatcherBuilderStep.java
+++ grpcmock-core/src/main/java/org/grpcmock/definitions/matcher/steps/StreamRequestMatcherBuilderStep.java
@@ -16,14 +16,14 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if all received stream requests satisfies given {@link Predicate}.
    * <p>Only applicable to client and bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   BUILDER withRequests(@Nonnull Predicate<List<ReqT>> requestsPredicate);
 
   /**
    * <p>Adds a requests' matcher, which checks if all received stream requests are equal to provided list.
    * <p>Only applicable to client and bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withRequests(@Nonnull List<ReqT> requests) {
     Objects.requireNonNull(requests);
@@ -33,7 +33,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if all received stream requests are equal to provided array.
    * <p>Only applicable to client and bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withRequests(@Nonnull ReqT... requests) {
     Objects.requireNonNull(requests);
@@ -43,7 +43,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if received stream requests number is equal to provided one.
    * <p>Only applicable to client or bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withNumberOfRequests(int size) {
     return withRequests(list -> list.size() == size);
@@ -52,7 +52,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if any of the received stream requests satisfies given {@link Predicate}.
    * <p>Only applicable to client and bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withRequestsContaining(@Nonnull Predicate<ReqT> requestPredicate) {
     Objects.requireNonNull(requestPredicate);
@@ -62,7 +62,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if any of the received stream requests is equal to given request object.
    * <p>Only applicable to client and bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withRequestsContaining(@Nonnull ReqT request) {
     Objects.requireNonNull(request);
@@ -72,7 +72,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if a request at given index satisfies given {@link Predicate}.
    * <p>Only applicable to client and bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withRequestAtIndex(int index, @Nonnull Predicate<ReqT> requestPredicate) {
     Objects.requireNonNull(requestPredicate);
@@ -82,7 +82,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if a request at given index is equal to given request object.
    * <p>Only applicable to client or bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withRequestAtIndex(int index, @Nonnull ReqT request) {
     Objects.requireNonNull(request);
@@ -92,7 +92,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if the first received request satisfies given {@link Predicate}.
    * <p>Only applicable to client or bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withFirstRequest(@Nonnull Predicate<ReqT> requestPredicate) {
     return withRequestAtIndex(0, requestPredicate);
@@ -101,7 +101,7 @@ public interface StreamRequestMatcherBuilderStep<BUILDER extends StreamRequestMa
   /**
    * <p>Adds a requests' matcher, which checks if the first received request is equal to given request object.
    * <p>Only applicable to client or bidi streaming methods.
-   * <p>Subsequent requests matchers will added as additional conditions.
+   * <p>Subsequent requests matchers will be added as additional conditions.
    */
   default BUILDER withFirstRequest(@Nonnull ReqT request) {
     Objects.requireNonNull(request);
