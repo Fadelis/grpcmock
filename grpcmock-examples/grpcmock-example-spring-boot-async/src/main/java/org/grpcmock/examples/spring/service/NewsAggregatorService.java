c6cf2d0058a20930571e4de7a57852715d8a815f
Fadelis <paulius.papl@gmail.com>
2021-01-24 13:05:37 +0100

    adds synchronized stream observer in async example

--- grpcmock-examples/grpcmock-example-spring-boot-async/src/main/java/org/grpcmock/examples/spring/service/NewsAggregatorService.java
+++ grpcmock-examples/grpcmock-example-spring-boot-async/src/main/java/org/grpcmock/examples/spring/service/NewsAggregatorService.java
@@ -3,6 +3,7 @@ package org.grpcmock.examples.spring.service;
 import io.grpc.stub.StreamObserver;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.locks.ReentrantLock;
 import org.grpcmock.examples.v1.GetNewsRequest;
 import org.grpcmock.examples.v1.GetNewsResponse;
 import org.grpcmock.examples.v1.NewsAggregatorServiceGrpc.NewsAggregatorServiceImplBase;
@@ -22,12 +23,41 @@ public class NewsAggregatorService extends NewsAggregatorServiceImplBase {
 
   @Override
   public void getAggregatedNews(GetNewsRequest request, StreamObserver<GetNewsResponse> responseObserver) {
+    StreamObserver<GetNewsResponse> synchronizedResponseObserver = synchronizedStreamObserver(responseObserver);
     CompletableFuture[] newsFutures = downstreamServices.stream()
         .map(downstreamNewsService -> downstreamNewsService.getNews(request)
-            .thenAccept(responseObserver::onNext))
+            .thenAccept(synchronizedResponseObserver::onNext))
         .toArray(CompletableFuture[]::new);
 
     CompletableFuture.allOf(newsFutures)
-        .whenComplete((ignore, exception) -> responseObserver.onCompleted());
+        .whenComplete((ignore, exception) -> synchronizedResponseObserver.onCompleted());
+  }
+
+  /**
+   * @return A wrapped synchronized stream observer, which can safely handle multiple concurrent writes to it.
+   */
+  public static <T> StreamObserver<T> synchronizedStreamObserver(StreamObserver<T> delegate) {
+    ReentrantLock lock = new ReentrantLock();
+    return new StreamObserver<T>() {
+      @Override
+      public void onNext(T value) {
+        lock.lock();
+        try {
+          delegate.onNext(value);
+        } finally {
+          lock.unlock();
+        }
+      }
+
+      @Override
+      public void onError(Throwable error) {
+        delegate.onError(error);
+      }
+
+      @Override
+      public void onCompleted() {
+        delegate.onCompleted();
+      }
+    };
   }
 }
